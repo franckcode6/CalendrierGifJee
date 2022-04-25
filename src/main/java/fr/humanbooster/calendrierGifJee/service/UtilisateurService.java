dda44e5fc3b945aed78ab9a09a4ce721b0c23067
Franckcode6 <franckcode6@gmail.com>
2022-04-25 19:36:36 +0200

    Ajout de UtilisateurService()

---
+++ src/main/java/fr/humanbooster/calendrierGifJee/service/UtilisateurService.java
@@ -0,0 +1,20 @@
+package fr.humanbooster.calendrierGifJee.service;
+
+import java.util.List;
+
+import fr.humanbooster.calendrierGifJee.business.Theme;
+import fr.humanbooster.calendrierGifJee.business.Utilisateur;
+
+public interface UtilisateurService {
+
+	public Utilisateur ajouterUtilisateur(String nom, String prenom, String email, String motDePasse, Theme theme);
+	
+	public List<Utilisateur> recupererUtilisateurs();
+	
+	public Utilisateur recupererUtilisateur(Long id);
+	
+	public Utilisateur recupererUtilisateur(String nom);
+	
+	public void supprimerUtilisateur(Long id);
+	
+}
---
+++ src/main/java/fr/humanbooster/calendrierGifJee/service/impl/UtilisateurServiceImpl.java
@@ -0,0 +1,70 @@
+package fr.humanbooster.calendrierGifJee.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.humanbooster.calendrierGifJee.business.Theme;
+import fr.humanbooster.calendrierGifJee.business.Utilisateur;
+import fr.humanbooster.calendrierGifJee.service.UtilisateurService;
+
+public class UtilisateurServiceImpl implements UtilisateurService {
+	
+	private static List<Utilisateur> utilisateurs = new ArrayList<>();
+
+	/**
+	 * Méthode permettant d'ajouter un utilisateur
+	 */
+	@Override
+	public Utilisateur ajouterUtilisateur(String nom, String prenom, String email, String motDePasse, Theme theme) {
+		Utilisateur utilisateur = new Utilisateur(nom, prenom, email, motDePasse, theme);
+		utilisateurs.add(utilisateur);
+		return utilisateur;
+	}
+
+	/**
+	 * Méthode permettant de récupérer la liste des utilisateurs
+	 */
+	@Override
+	public List<Utilisateur> recupererUtilisateurs() {
+		return utilisateurs;
+	}
+
+	/**
+	 * Méthode permettant de récupérer un utilisateur par id
+	 */
+	@Override
+	public Utilisateur recupererUtilisateur(Long id) {
+		for (Utilisateur utilisateur : utilisateurs) {
+			if (utilisateur.getId().equals(id)) {
+				return utilisateur;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Méthode permettant de récupérer un utilisateur par nom
+	 */
+	@Override
+	public Utilisateur recupererUtilisateur(String nom) {
+		for (Utilisateur theme : utilisateurs) {
+			if (theme.getNom().equals(nom)) {
+				return theme;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Méthode permettant de supprimer un utilisateur par son id
+	 */
+	public void supprimerUtilisateur(Long id) {
+		for (Utilisateur theme : utilisateurs) {
+			if (theme.getId().equals(id)) {
+				utilisateurs.remove(theme);
+			}
+		}
+
+	}
+
+}
